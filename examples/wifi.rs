b08e086da3a7d48dd6f2bd405a6277f5081c9b27
Samuel Benko <samuel.benko@espressif.com>
2022-09-20 17:01:15 +0200

    initial commit (failed linking error //)

--- examples/wifi.rs
+++ examples/wifi.rs
@@ -9,16 +9,42 @@ use embedded_graphics::{
     prelude::*,
     text::{Baseline, Text},
 };
+
+use embedded_io::blocking::*;
+use embedded_svc::wifi::{
+    AccessPointInfo, ClientConfiguration, ClientConnectionStatus, ClientIpStatus, ClientStatus,
+    Configuration, Status, Wifi,
+};
+
 use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
 use esp32_hal::{clock::ClockControl, i2c, IO, pac::Peripherals, prelude::*, timer::TimerGroup, Rtc};
 use esp_backtrace as _;
 use xtensa_lx_rt::entry;
 
+use esp_println::{print, println};
+use esp_wifi::wifi::utils::{create_network_interface, Network};
+use esp_wifi::wifi_interface::{timestamp, WifiError};
+use esp_wifi::{create_network_stack_storage, network_stack_storage};
+use esp_wifi::{current_millis, initialize};
+
+use smoltcp::wire::Ipv4Address;
+
+extern crate alloc;
+
+const SSID: &str = env!("SSID");
+const PASSWORD: &str = env!("PASSWORD");
+
 #[entry]
 fn main() -> ! {
+    esp_wifi::init_heap();
+
     let peripherals = Peripherals::take().unwrap();
     let mut system = peripherals.DPORT.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let mut storage = create_network_stack_storage!(3, 8, 1);
+    let ethernet = create_network_interface(network_stack_storage!(storage));
+    let mut _wifi_interface = esp_wifi::wifi_interface::Wifi::new(ethernet);
 
     // Disable the RTC and TIMG watchdog timers
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
@@ -26,7 +52,6 @@ fn main() -> ! {
     let mut wdt0 = timer_group0.wdt;
     let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
     let mut wdt1 = timer_group1.wdt;
-
 
     rtc.rwdt.disable();
     wdt0.disable();
@@ -68,6 +93,35 @@ fn main() -> ! {
     //     .draw(&mut display)
     //     .unwrap();
 
+
+    println!("{:?}", _wifi_interface.get_status());
+
+    println!("Start Wifi Scan");
+    // let res: Result<(heapless::Vec<AccessPointInfo, 10>, usize), WifiError> =
+    //     _wifi_interface.scan_n();
+    // if let Ok((res, _count)) = res {
+    //     for ap in res {
+    //         println!("{:?}", ap);
+    //     }
+    // }
+
+    println!("Call wifi_connect");
+    let client_config = Configuration::Client(ClientConfiguration {
+        ssid: SSID.into(),
+        password: PASSWORD.into(),
+        ..Default::default()
+    });
+
+    Text::new("Connected.", Point::new(0, 20), text_style)
+        .draw(&mut display)
+        .unwrap();
+
+    let res = _wifi_interface.set_configuration(&client_config);
+    //println!("wifi_connect returned {:?}", res);
+
+    //println!("{:?}", _wifi_interface.get_capabilities());
+    //println!("{:?}", _wifi_interface.get_status());
+    
     display.flush().unwrap();
 
     loop {}
